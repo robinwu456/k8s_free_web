f37e0bf6a5f6ca2059d965d48ef0da957aacfd8d
danny <danny@example.com>
2022-09-29 15:59:13 +0800

    dynamic update service ip test 19

--- replace_ip.sh
+++ replace_ip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mysql_ip=$(kubectl get service -n bliss-prod | grep mysql | awk '{print $3}')
-echo $mysql_ip
+redis_ip=$(kubectl get service -n bliss-prod | grep redis | awk '{print $3}')
 
 sed -i s/NNF_DB_HOST=10.98.0.254/NNF_DB_HOST=$mysql_ip/g free_web.env
+sed -i s/NNF_REDIS_HOST=10.98.0.118/NNF_REDIS_HOST=$redis_ip/g free_web.env
+
+cat free_web.env
